ca2532558f05e88d91ec8928d05cbbaa1f48ea5d
kthawani <kthawani@entera.ai>
2023-10-18 15:49:41 +0530

    styling added

---
+++ components/layout/logo.js
@@ -0,0 +1,8 @@
+import { Fragment } from "react";
+import classes from "./logo.module.css";
+
+function Logo() {
+  return <div className={classes.logo}>Kamlesh' Next Blog</div>;
+}
+
+export default Logo;
