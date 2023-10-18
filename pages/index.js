270e048d61edb5a430378e8ed9a4e2fe5c4c6cb0
kthawani <kthawani@entera.ai>
2023-10-18 17:12:48 +0530

    Rendering Dummy Post Data

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,42 @@ import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
 import FeaturedPost from "../components/home-page/featured-posts";
 
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is a the React framework for Production.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is a the React framework for Production.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is a the React framework for Production.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is a the React framework for Production.",
+    date: "2022-02-10",
+  },
+];
+
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPost />
+      <FeaturedPost posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
