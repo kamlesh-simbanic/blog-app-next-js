0d2a977e5d11e75f2d90ddbe85c887f1d112a07c
kthawani <kthawani@entera.ai>
2023-10-20 15:17:30 +0530

    markdown added

--- pages/posts/post-detail/post-content.js
+++ pages/posts/post-detail/post-content.js
@@ -1,3 +1,5 @@
+import ReactMarkDown from "react-markdown";
+
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 
@@ -15,7 +17,7 @@ function PostContent() {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkDown>{DUMMY_POST.content}</ReactMarkDown>
     </article>
   );
 }
