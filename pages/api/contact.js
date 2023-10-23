7d7547d05360668ccb63b291d8ea04cb28a95fde
kthawani <kthawani@entera.ai>
2023-10-23 17:22:27 +0530

    storing data

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,14 @@
-function Handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://kamlesh_1997:kamlesh_1997@shah-collections.1k1pn.mongodb.net/my-site?retryWrites=true&w=majority"
+  );
+
+  return client;
+}
+
+async function Handler(req, res) {
   if (req.method == "POST") {
     console.log(req.body);
     const { email, name, message } = req.body;
@@ -21,7 +31,27 @@ function Handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("message").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing Message failed" });
+      return;
+    }
+
+    client.close();
 
     res
       .status(201)
