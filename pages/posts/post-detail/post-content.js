8a087ee25440d11d7ad930a8f0909cbeb714fcbb
kthawani <kthawani@entera.ai>
2023-10-23 15:11:57 +0530

    Rendering image with the 'Next Image'

--- pages/posts/post-detail/post-content.js
+++ pages/posts/post-detail/post-content.js
@@ -2,6 +2,7 @@ import ReactMarkDown from "react-markdown";
 
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
+import Image from "next/image";
 
 const DUMMY_POST = {
   slug: "getting-started-with-nextjs",
@@ -16,10 +17,45 @@ function PostContent(props) {
   const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkDown>{post.content}</ReactMarkDown>
+      <ReactMarkDown components={customRenderers}>{post.content}</ReactMarkDown>
     </article>
   );
 }
